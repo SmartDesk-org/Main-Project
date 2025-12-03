a3efc49da4396a345dfb98804a1722b6b8503e98
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-12-03 16:04:51 +0530

    User and role entity , DB context , configuration and seed added by suhail

---
+++ ResourceFlow.Infrastructure/Persistence/EF/Configurations/SeedData/UserSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ResourceFlow.Domain.Entities.Authntication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceFlow.Infrastructure.Persistence.EF.Configurations.SeedData
+{
+    internal class UserSeed:IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            string password = BCrypt.Net.BCrypt.HashPassword("123456");
+            builder.HasData(
+                new User {UserId=1, Email="suhailpalakkal1@gmail.com",PassWord=password,RoleId=1}
+                );
+        }
+    }
+}
---
+++ ResourceFlow.WebAPI/DI/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResourceFlow.Infrastructure.Persistence.EF.Context;
+using System.Runtime.CompilerServices;
+
+namespace ResourceFlow.WebAPI.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services,IConfiguration config)
+        {
+
+            // ----------- DbContext (EF Core) -----------
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            });
+
+
+            // ----------- Repositories -----------
+
+
+
+            // ----------- Services -----------
+
+            return services;
+        }
+    }
+}
