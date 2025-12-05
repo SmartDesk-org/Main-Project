6f284a8759c069773f3b429791aa579f59753828
rinsh4dd <rinshadcontacts@gmail.com>
2025-12-05 12:02:56 +0530

    Fix: DI updates

---
+++ ResourceFlow.WebAPI/Di/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResourceFlow.Infrastructure.Persistence.EF.Context;
+
+namespace ResourceFlow.WebAPI.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProjectServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            // Register Database Context
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+            );
+
+
+
+            // Register Repositories
+            
+            // Add all your repositories here...
+
+
+
+            // Register Services
+            
+            // Add all your services here...
+
+
+
+            return services;
+        }
+    }
+}
