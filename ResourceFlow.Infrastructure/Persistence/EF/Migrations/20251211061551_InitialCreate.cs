19dac4d38520877691ac08a275ad386c8f843ec9
Your Name <marjanmuhammad790@gmail.com>
2025-12-11 19:08:24 +0530

    Updated appsettings and added new EF migration

---
+++ ResourceFlow.Infrastructure/Persistence/EF/Migrations/20251211061551_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResourceFlow.Infrastructure.Persistence.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 11, 6, 15, 49, 682, DateTimeKind.Utc).AddTicks(4385));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 11, 6, 15, 49, 682, DateTimeKind.Utc).AddTicks(4394));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 11, 6, 15, 49, 682, DateTimeKind.Utc).AddTicks(4396));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "CreateAt", "PassWord" },
+                values: new object[] { new DateTime(2025, 12, 11, 6, 15, 49, 903, DateTimeKind.Utc).AddTicks(1927), "$2a$11$GXFlaxXZ5aPcuT46FnVqL.47Ui5NxLb.jVEYCiXi87/IBPmRHSKaK" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 8, 7, 21, 19, 361, DateTimeKind.Utc).AddTicks(6634));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 8, 7, 21, 19, 361, DateTimeKind.Utc).AddTicks(6639));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateAt",
+                value: new DateTime(2025, 12, 8, 7, 21, 19, 361, DateTimeKind.Utc).AddTicks(6640));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "CreateAt", "PassWord" },
+                values: new object[] { new DateTime(2025, 12, 8, 7, 21, 19, 726, DateTimeKind.Utc).AddTicks(1063), "$2a$11$8vaDk1cb0B3xDDJu9Swvu.kYZtZ7ltBAeZ5xO3i2Lyt0P3wlkQWsW" });
+        }
+    }
+}
