9792e61e69a1bffdbb09bd7b1a3075f1969ed37e
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-12-23 16:26:02 +0530

    some changes

--- ResourceFlow.WebAPI/Program.cs
+++ ResourceFlow.WebAPI/Program.cs
@@ -7,8 +7,6 @@ using System.Text;
 using System.Threading.RateLimiting;
 using OfficeOpenXml;
 using ResourceFlow.WebAPI.Middleware;
-<<<<<<< HEAD
-=======
 using Serilog;
 
 
@@ -24,7 +22,7 @@ Log.Logger = new LoggerConfiguration()
 
 
 
->>>>>>> 671942c9e2248c9f918dc76bd4807271fad51a93
+
 
 var builder = WebApplication.CreateBuilder(args);
 
